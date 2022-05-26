e4d5471b8a7b1496102f892fbef6c940c6fdcb47
Vardhan1234 <vardhan981442@gmail.com>
2022-05-26 16:56:39 +0530

    first basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Material(
-
-        child: Container(
-          child: Text("Welcome"),
-      ),
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome"),
+          ),
+        ),
       ),
     );
   }
